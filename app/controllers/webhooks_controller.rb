e86026178bbd7b29391cd1502cfab282114cf792
grandmasterspoc <dilloncortez@gmail.com>
2016-06-18 17:43:03 -0600

    added action for viewing recieved data

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,13 +1,13 @@
 class WebhooksController < ApplicationController
 
   def index
-    
+    @webhooks = Webhook.all
   end  
 
 
   def call_center_response
-    full_data = params[:my_data]
-    Webhook.create(fulldata: full_data)
+    # full_data = params[:my_data]
+    # Webhook.create(fulldata: full_data)
     head 200
   end  
 end
