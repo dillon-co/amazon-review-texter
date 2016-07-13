bc8151a34f9380d53fa8e664d9803745be46cf13
grandmasterspoc <dilloncortez@gmail.com>
2016-07-13 17:06:06 -0600

    fixed bugs and cron job

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -1,1 +1,1 @@
-Sidekiq::Cron::Job.create(name:"grab orders from 2 weeks ago", cron: "0 2 * * *", klass: 'DailyNameGenerator')
+Sidekiq::Cron::Job.create(name:"grab orders from 2 weeks ago", cron: "0 9 * * *", klass: 'DailyNameGenerator')
