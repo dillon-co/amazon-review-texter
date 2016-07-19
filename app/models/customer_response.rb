6be6520b9945011fcce6070e914f0ad08bc93719
grandmasterspoc <dilloncortez@gmail.com>
2016-07-19 13:36:40 -0400

    fixed relation

--- app/models/customer_response.rb
+++ app/models/customer_response.rb
@@ -12,15 +12,16 @@
 
 class CustomerResponse < ActiveRecord::Base
 
-  belongs_to :customer
+  belongs_to :client
   has_many :messages
+
   after_save :forward_message
 
 
   def forward_message
     if client.phone_number == '8056038329'
       user_name, full_message = message.split(' - ', 2)
-      c = client.find_by(name: user_name)
+      
       message.new(user_number: client.phone_number, sms_messaage: full_message)
     else  
       message.new(user_number: '8056038329', sms_message: clients_message)
