710d944e5a4fc56fa87b3c8ccabf8709fbd6a8f3
grandmasterspoc <dilloncortez@gmail.com>
2016-07-19 14:08:03 -0400

    fixed typo

--- app/models/customer_response.rb
+++ app/models/customer_response.rb
@@ -22,7 +22,7 @@ class CustomerResponse < ActiveRecord::Base
     if client.phone_number == '+18056038329'
       users_id, full_message = message.split(' - ', 2)
       c = Client.find(users_id.to_i)
-      messages.create(user_number: c.phone_number, sms_messaage: full_message)
+      messages.create(user_number: c.phone_number, sms_message: full_message)
     else  
       messages.create(user_number: '8056038329', sms_message: clients_message)
     end  
