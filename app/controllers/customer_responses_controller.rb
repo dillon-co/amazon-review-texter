703be28741c07ee2a3622e371a7b5b808376ac8e
grandmasterspoc <dilloncortez@gmail.com>
2016-07-19 13:27:28 -0400

    added head 200

--- app/controllers/customer_responses_controller.rb
+++ app/controllers/customer_responses_controller.rb
@@ -4,5 +4,6 @@ class CustomerResponsesController < ApplicationController
   def new_response
     c = Client.find_or_create_by(phone_number: params["From"])
     c.customer_response.create(message: params['Body'])
+    head 200
   end  
 end
