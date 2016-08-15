fe372016ac5773447a9c017a5262ed7827b31358
grandmasterspoc <dilloncortez@gmail.com>
2016-08-15 13:50:38 -0600

    update cron job time

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -1,3 +1,3 @@
-Sidekiq::Cron::Job.create(name:"grab orders from 2 weeks ago", cron: "0 7 * * *", klass: 'DailyNameGenerator')
+Sidekiq::Cron::Job.create(name:"grab orders from 2 weeks ago", cron: "0 9 * * *", klass: 'DailyNameGenerator')
 
-Sidekiq::Cron::Job.create(name:"grab names from 3 days ago", cron: "0 4 * * *", klass: 'DailyEmailCreator')
+Sidekiq::Cron::Job.create(name:"grab names from 3 days ago", cron: "0 10 * * *", klass: 'DailyEmailCreator')
